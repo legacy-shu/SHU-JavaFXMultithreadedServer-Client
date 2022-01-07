3e68c7dfdcd96cecdde699f5c3cb61a4d0a9ea66
w-ryan-jung <w.ryan.jung@gmail.com>
2022-01-07 01:18:57 +0100

    add

--- SHUNoticeBoardServer/src/shu/nsd/Main.java
+++ SHUNoticeBoardServer/src/shu/nsd/Main.java
@@ -3,6 +3,7 @@ package shu.nsd;
 import java.io.IOException;
 import java.net.ServerSocket;
 
+// add
 public class Main {
     public static void main(String[] args) throws IOException {
         Server server = new Server(new ServerSocket(12345));
